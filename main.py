3e2de18ed5b327a33735d46c9e247fd68a3a398e
Rohit Purkait <codeswithroh@gmail.com>
2023-12-02 06:52:48 +0000

    works locally

--- main.py
+++ main.py
@@ -18,32 +18,30 @@ def get_default_download_path():
         raise Exception(f"Unsupported operating system: {system}")
 
 
-def download_video(url):
+def download_video(url,download_path):
     try:
         # Display loading spinner while downloading
         with st.spinner("Downloading..."):
             yt = YouTube(url)
             video = yt.streams.get_highest_resolution()
-            video.download()
+            video.download(download_path)
 
-        # Remove "Downloading..." text after download is complete
-        # st.info("")
         st.success("Download complete!")
     except Exception as e:
         st.error(f"Error: {str(e)}")
 
 
+
 def main():
     st.title("YouTube Video Downloader")
 
-    # Input for YouTube URL
     url = st.text_input("Enter YouTube Video URL:")
 
-    # download_path = get_default_download_path()
+    download_path = get_default_download_path()
 
     if st.button("Download"):
         if url:
-            download_video(url)
+            download_video(url, download_path)
         else:
             st.warning(
                 "Please provide the YouTube URL")
